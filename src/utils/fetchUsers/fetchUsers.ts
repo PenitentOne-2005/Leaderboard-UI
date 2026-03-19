947bc7bc6b80da55b0469dd28be7ae83e2297265
PenitentOne-2005 <pozdrakovzena@gmail.com>
2026-03-20 00:33:08 +0300

    Update for infinity scroll

--- src/utils/fetchUsers/fetchUsers.ts
+++ src/utils/fetchUsers/fetchUsers.ts
@@ -1,11 +1,25 @@
-import { generateUsers } from "@/utils";
-
-const allUsers = generateUsers(2000);
+const names = [
+  "Катерина Петренко Катерина Петренко",
+  "Олексій Іванов",
+  "Марія Коваль",
+  "Володимир Сидоренко Володимир Сидоренко",
+  "Олена Шевченко",
+  "Дмитро Бондаренко",
+  "Світлана Лі",
+  "Ігор Кравченко",
+  "Анна Мельник",
+];
 
 const fetchUsers = async (start: number, limit: number) => {
   await new Promise((r) => setTimeout(r, 700));
 
-  return allUsers.slice(start, start + limit);
+  return Array.from({ length: limit }, (_, i) => ({
+    id: start + i,
+    name: names[Math.floor(Math.random() * names.length)],
+    speed: Math.floor(Math.random() * 20) + 80,
+    time: Math.floor(Math.random() * 120000),
+    color: Math.floor(Math.random() * 3),
+  }));
 };
 
 export default fetchUsers;
